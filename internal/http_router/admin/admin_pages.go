00c081dbdab5e89bbec12bd5d5de3cc29bd33497
Sasha <61081677+moneyharassment@users.noreply.github.com>
2022-09-04 20:33:50 +0300

    Statistic

--- internal/http_router/admin/admin_pages.go
+++ internal/http_router/admin/admin_pages.go
@@ -2,11 +2,32 @@ package admin
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"template/internal/models"
 	"template/pgk/memcache"
+	"template/pgk/session_manager"
 )
 
 func Homepage(c *fiber.Ctx) error {
-	return c.Render("index", fiber.Map{})
+	user := session_manager.GetUser(c)
+	var keys []models.KeyModel
+	var keysBanned []models.KeyModel
+	var Actived int
+
+	if user.Status == "seller" {
+		keys = memcache.KeyCache.SellerNotBanned(user.Username)
+		keysBanned = memcache.KeyCache.SellerBanned(user.Username)
+		Actived = memcache.KeyCache.SellerActived(user.Username)
+	} else {
+		keys = memcache.KeyCache.NotBanned()
+		keysBanned = memcache.KeyCache.Banned()
+		Actived = memcache.KeyCache.Actived()
+	}
+
+	return c.Render("index", fiber.Map{
+		"Created": len(keys),
+		"Banned":  len(keysBanned),
+		"Actived": Actived,
+	})
 }
 
 func Users(c *fiber.Ctx) error {
@@ -23,15 +44,33 @@ func Cheats(c *fiber.Ctx) error {
 }
 
 func Keys(c *fiber.Ctx) error {
+	user := session_manager.GetUser(c)
+	var keys []models.KeyModel
+
+	if user.Status == "seller" {
+		keys = memcache.KeyCache.SellerNotBanned(user.Username)
+	} else {
+		keys = memcache.KeyCache.NotBanned()
+	}
+
 	return c.Render("keys", fiber.Map{
-		"Keys":   memcache.KeyCache.NotBanned(),
+		"Keys":   keys,
 		"Cheats": memcache.CheatCache,
 	})
 }
 
 func KeysBanned(c *fiber.Ctx) error {
-	return c.Render("keys-banned", fiber.Map{
-		"Keys":   memcache.KeyCache.Banned(),
+	user := session_manager.GetUser(c)
+	var keys []models.KeyModel
+
+	if user.Status == "seller" {
+		keys = memcache.KeyCache.SellerBanned(user.Username)
+	} else {
+		keys = memcache.KeyCache.Banned()
+	}
+
+	return c.Render("keys", fiber.Map{
+		"Keys":   keys,
 		"Cheats": memcache.CheatCache,
 	})
 }
--- pgk/memcache/key_cache.go
+++ pgk/memcache/key_cache.go
@@ -20,6 +20,48 @@ func (c keyModelCache) Get(keycode string) models.KeyModel {
 	return models.KeyModel{}
 }
 
+func (c keyModelCache) Actived() (u int) {
+	u = 0
+	for _, a := range c {
+		if a.Status == 1 {
+			u += 1
+		}
+	}
+
+	return
+}
+
+func (c keyModelCache) SellerActived(Seller string) (u int) {
+	u = 0
+	for _, a := range c {
+		if a.CreatedBy == Seller && a.Status == 1 {
+			u += 1
+		}
+	}
+
+	return
+}
+
+func (c keyModelCache) SellerBanned(Seller string) (u []models.KeyModel) {
+	for _, a := range c {
+		if a.CreatedBy == Seller && a.Banned == true {
+			u = append(u, a)
+		}
+	}
+
+	return
+}
+
+func (c keyModelCache) SellerNotBanned(Seller string) (u []models.KeyModel) {
+	for _, a := range c {
+		if a.Banned == false && a.CreatedBy == Seller {
+			u = append(u, a)
+		}
+	}
+
+	return
+}
+
 func (c keyModelCache) NotBanned() (u []models.KeyModel) {
 	for _, a := range c {
 		if a.Banned == false {
